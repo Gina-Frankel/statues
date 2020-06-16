f0d684b4f9f1634ddbe2b274ef92c5792dc518c2
Gina Frankel <g.frankel22@gmail.com>
2020-06-16 17:55:00 +0100

    Test: sanity check

--- spec/array_to_csv_spec.rb
+++ spec/array_to_csv_spec.rb
@@ -2,9 +2,16 @@ require 'array_to_csv'
 require 'csv'
 
 describe 'array_to_csv' do
-  it 'can turn a array  to  a csv file' do
+  it 'can turn an array to a csv file' do
     array = ['gina']
     array_to_csv(array)
     expect(CSV.read(__dir__ + '/../results.csv')).to eq [array]
-  end 
-end
+  end
+
+  it 'can turn an array with multiple elements into rows' do 
+    array = ['gina', 'harry']
+    array_to_csv(array)
+    expect(CSV.read(__dir__ + '/../results.csv')).to eq [['gina'],['harry']]
+  end
+end 
+
