23b77704e4c3a7ef8e0175de49171ff70d2ffd97
Gina Frankel <g.frankel22@gmail.com>
2020-06-16 16:35:48 +0100

    Add: #extract_links_from_string so it returns link as an element of an array

---
+++ lib/data_extractor.rb
@@ -0,0 +1,3 @@
+def data_extractor(string)
+  ['https://artuk.org/discover/artworks/figurine-of-two-elephants-273650/']
+end 
---
+++ spec/data_extractor_spec.rb
@@ -0,0 +1,9 @@
+require 'data_extractor'
+
+describe 'data_extractor' do
+
+  it 'can find a link ' do
+    string = '<li class="item artwork icons visible" style="position: absolute; left: 3.94089%; top: 586515px;"><a id="figurine-of-two-elephants-273650" class="" href="https://artuk.org/discover/artworks/figurine-of-two-elephants-273650/search/work_type:sculpturestatue-1888609/page/105" title="View artwork"></a><div class="wrap"><div class="img-wrap"><img src="https://artuk.org/skins/artuk/img/placeholder-artwork-listing.png"> <div class="actions "> <div class="action-toggle"> <a class="toggle fav ajax-action no-user " data-add-fav="" data-action="fav-artwork" href="https://artuk.org/favourite/figurine-of-two-elephants-273650" data-toggle="tooltip-hover" data-placement="top" title="" data-original-title="Sign in to favourite" data-title-add="Add to favourites" data-title-remove="Remove from favourites"></a> </div><div class="album action-toggle toolbar_icon no-user" data-action-dropdown=""> <div class="toggle album toolbar_icon" data-toggle="tooltip-hover" data-placement="top" title="Sign in to curate" data-original-title="Sign in to curate"> </div></div></div></div></div><div class="info"> <div class="icons"> </div><span class="title"> Figurine of Two Elephants </span> <span class="artist"> unknown artist </span> </div><div class="more-info"> <span class="title"> Figurine of Two Elephants </span> <span class="artist"> University of Birmingham </span> </div></li>'
+    expect(data_extractor(string)).to eq ['https://artuk.org/discover/artworks/figurine-of-two-elephants-273650/']
+  end
+end
