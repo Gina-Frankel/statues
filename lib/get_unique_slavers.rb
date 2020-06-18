9c9a4fe74af638625b453d460a9d725f723a478d
Harry Turnbull <hturnbull@live.co.uk>
2020-06-18 11:35:44 +0100

    G: splits out array elements that have "<br/> " separators

--- lib/get_unique_slavers.rb
+++ lib/get_unique_slavers.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
+require_relative 'split_br.rb'
 
 def get_unique_slavers(array)
-  array.uniq
+  split_elements = array.map do |element|
+    split_br(element)
+  end
+  split_elements.flatten.uniq
 end
