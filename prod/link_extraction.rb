52e815a32fd07f132ef7738344691d3aa03c9488
Harry Turnbull <hturnbull@live.co.uk>
2020-06-18 11:51:17 +0100

    Lint

--- prod/link_extraction.rb
+++ prod/link_extraction.rb
@@ -1,7 +1,7 @@
 require_relative '../lib/data_extractor'
 require_relative '../lib/array_to_csv'
 
-file = File.read(__dir__ + '/../data.txt' )
+file = File.read(__dir__ + '/../data.txt')
 
 result = extract_links_from_string(file)
 
