0e15487a36161ca0f8dba7ed6addba89bf3a143c
Gina Frankel <g.frankel22@gmail.com>
2020-06-30 15:25:22 +0100

    Remove boiler plate

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-
-          Learn React
-        </a>
-  
-        
-        <Button className="my-3 py-3"> submit me</Button>
+        hello world
       </header>
     </div>
   );
