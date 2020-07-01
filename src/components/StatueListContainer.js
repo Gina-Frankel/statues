fe7bafa1ffe1d90fc450123d0594c5d6fdcad664
Gina Frankel <g.frankel22@gmail.com>
2020-07-01 21:45:20 +0100

    Import JS object + pass it to child component

--- src/components/StatueListContainer.js
+++ src/components/StatueListContainer.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react'
 import StatueList from "./Statuelist"
+import SlaverStatues from "../Statues/slavers"
+
+
 
 export class StatueListContainer extends Component {
+  constructor(props){
+    super(props);
+    this.state ={
+      statues: SlaverStatues
+    }
+  }
+
+  // consoleLog = () => {
+  //   console.dir(this.state.statues)
+  // }
+
+
+  
   render() {
     return (
       <div>
         <h1>Statues</h1>
-        <StatueList/>
+        {/* {this.state.statues[0].statueName} */}
+        <StatueList 
+          statue={this.state.statues}/>
       </div>
     )
   }
 }
+
+
 
 export default StatueListContainer
