ebd04d75c6b70fbaf74c3bb7bab6d70928bcf12a
Gina Frankel <g.frankel22@gmail.com>
2020-07-01 17:57:54 +0100

    Add parent directory

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import StatueListContainer from './StatueListContainer.js'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <div>
+        <StatueListContainer/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/components/StatueListContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class StatueListContainer extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Statues</h1>
+      </div>
+    )
+  }
+}
+
+export default StatueListContainer
