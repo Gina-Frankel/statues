c3711378a060cf2629b513416d691c7714c91132
Gina Frankel <g.frankel22@gmail.com>
2020-07-01 19:52:44 +0100

    Import StatueList  component + render

--- src/components/StatueListContainer.js
+++ src/components/StatueListContainer.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import StatueList from "./Statuelist"
 
 export class StatueListContainer extends Component {
   render() {
     return (
       <div>
         <h1>Statues</h1>
+        <StatueList/>
       </div>
     )
   }
