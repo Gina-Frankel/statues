aabeb23d63dba5025960efb614c2eee0ba2932a8
Gina Frankel <g.frankel22@gmail.com>
2020-07-01 21:49:37 +0100

    Delete comments + extra spaces

--- src/components/StatueListContainer.js
+++ src/components/StatueListContainer.js
@@ -1,35 +1,23 @@
-import React, { Component } from 'react'
-import StatueList from "./Statuelist"
-import SlaverStatues from "../Statues/slavers"
-
-
+import React, { Component } from "react";
+import StatueList from "./Statuelist";
+import SlaverStatues from "../Statues/slavers";
 
 export class StatueListContainer extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state ={
-      statues: SlaverStatues
-    }
+    this.state = {
+      statues: SlaverStatues,
+    };
   }
 
-  // consoleLog = () => {
-  //   console.dir(this.state.statues)
-  // }
-
-
-  
   render() {
     return (
       <div>
         <h1>Statues</h1>
-        {/* {this.state.statues[0].statueName} */}
-        <StatueList 
-          statue={this.state.statues}/>
+        <StatueList statue={this.state.statues} />
       </div>
-    )
+    );
   }
 }
 
-
-
-export default StatueListContainer
+export default StatueListContainer;
