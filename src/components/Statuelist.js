21e42938980e7f6bfff0175a13f4c6e1dd13121f
Gina Frankel <g.frankel22@gmail.com>
2020-07-02 12:06:55 +0100

    Delete comments

--- src/components/Statuelist.js
+++ src/components/Statuelist.js
@@ -1,10 +1,28 @@
 import React, { Component } from "react";
 
 export class StatueList extends Component {
+  constructor() {
+    super()
+    this.state = {
+   
+    }
+  }
+
+
   render() {
+    
+    const items = this.props.statue.map ((item,key) =>
+     <li key={item.statueName}>
+       {item.statueName}
+     </li>)
+
+    console.log(items)
     return (
       <div>
         <p>{this.props.statue[0].statueName}</p>
+        <ul>
+          {items}
+        </ul>
       </div>
     );
   }
