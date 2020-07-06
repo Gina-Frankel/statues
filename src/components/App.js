d4ddaf1bcbcd810233260185e48e271d7479b961
Sophia Bell <sophbell87@gmail.com>
2020-07-06 17:32:34 +0100

    styling for nav bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Heading from './Heading.js'
 import StatueListContainer from './StatueListContainer.js'
-// import './App.css';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="app">
+      <header className="app-header">
         <Heading/>
       </header>
       <div>
