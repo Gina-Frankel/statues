7459db3ea6cf7915555d9723e88c3eca9367c460
Gina Frankel <g.frankel22@gmail.com>
2020-07-07 16:11:23 +0100

    Test if it returns that name of one statue

--- src/components/StatueListItem.spec.js
+++ src/components/StatueListItem.spec.js
@@ -6,3 +6,13 @@ it ("renders without crashing", () => {
   const wrapper = shallow(<StatueListItem />)
   expect(wrapper).toHaveLength(1)
 })
+
+it ("returns the name of one statue", () => {
+  const MockSlaverStatues = [
+    {
+      name: "Edward Colston",
+    },
+  ];
+  const wrapper = shallow(<StatueListItem statue={MockSlaverStatues}/>);
+  expect(wrapper.text()).toContain("Edward Colston")
+})
