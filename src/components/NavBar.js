9151c85c35fc4b663bced504f1f8445ae28367e2
Gina Frankel <g.frankel22@gmail.com>
2020-07-07 14:17:30 +0100

    Change css file name

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import './Heading.css';
+import './NavBar.css';
 
 
 function NavBar() {
   return (
     <div className="NavBar">
     <nav className="navbar navbar-expand-lg heading">
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div className="navbar-nav">
-        <a className="nav-item nav-link" href="#">About</a>
-        <a className="nav-item nav-link" href="#">Statues</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <a className="nav-item nav-link" href="#">About</a>
+          <a className="nav-item nav-link" href="#">Statues</a>
+        </div>
       </div>
-    </div>
     </nav>
     </div>
   );
