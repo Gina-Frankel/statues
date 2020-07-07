3ce328a2fdec6f0dcfe611c2d7bddd79e498ee13
Gina Frankel <g.frankel22@gmail.com>
2020-07-07 22:50:31 +0100

    Fix test setup so compoment takes props

--- src/components/StatueListItem.spec.js
+++ src/components/StatueListItem.spec.js
@@ -1,18 +1,59 @@
 import React from "react";
 import { shallow } from "enzyme";
-import { StatueListItem } from "./StatueListItem"
+import { StatueListItem } from "./StatueListItem";
 
-it ("renders without crashing", () => {
-  const wrapper = shallow(<StatueListItem />)
-  expect(wrapper).toHaveLength(1)
-})
-
-it ("returns the name of one statue", () => {
+it("renders without crashing", () => {
   const MockSlaverStatues = [
     {
       name: "Edward Colston",
     },
   ];
-  const wrapper = shallow(<StatueListItem statue={MockSlaverStatues}/>);
-  expect(wrapper.text()).toContain("Edward Colston")
-})
+
+  const wrapper = shallow(<StatueListItem value={MockSlaverStatues} />);
+  expect(wrapper).toHaveLength(1);
+});
+
+it("returns the name of statue - Edward Colston", () => {
+  const MockSlaverStatues = {
+    name: "Edward Colston",
+  };
+  const wrapper = shallow(
+    <StatueListItem key={MockSlaverStatues.name} value={MockSlaverStatues} />
+  );
+  expect(wrapper.text()).toContain("Edward Colston");
+});
+
+it("returns the name of statue - Marquis de Lafayette ", () => {
+  const MockSlaverStatues = {
+    name: "Marquis de Lafayette",
+  };
+  const wrapper = shallow(
+    <StatueListItem key={MockSlaverStatues.name} value={MockSlaverStatues} />
+  );
+  expect(wrapper.text()).toContain("Marquis de Lafayette");
+});
+
+// // it("renders name of statue", () => {
+// //   const MockSlaverStatues = [
+// //     {
+// //       name: "Edward Colston",
+// //     },
+// //   ];
+// //   const wrapper = shallow(<StatueList statue={MockSlaverStatues} />);
+
+// //   expect(wrapper.text()).toContain("Edward Colston");
+// // });
+
+// // it("renders names of multiple statues", () =>{
+// //   const MockSlaverStatues = [
+// //     {
+// //       name: "Edward Colston",
+// //     },
+// //     {
+// //       name: "Marquis de Lafayette",
+// //     }
+// //   ]
+// //   const wrapper = shallow (<StatueList statue={MockSlaverStatues} />);
+// //   expect(wrapper.text()).toContain("Edward Colston")
+// //   expect(wrapper.text()).toContain("Marquis de Lafayette")
+// // })
