d3c9613286eeba22e2ccde472b00544e67acd111
Gina Frankel <g.frankel22@gmail.com>
2020-07-07 14:14:51 +0100

    Change className

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import './Heading.css';
 
 function NavBar() {
   return (
-    <div className="heading">
+    <div className="NavBar">
     <nav className="navbar navbar-expand-lg heading">
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
