c336db5eba845e08254f1177873f53dc1d6bd4f0
Gina Frankel <g.frankel22@gmail.com>
2020-07-08 21:49:39 +0100

    Delete as file name has changed

--- src/components/NavBar.spec.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { shallow } from "enzyme";
-import { NavBar } from "./NavBar";
-
-it("renders correctly", () => {
-  const wrapper = shallow(<NavBar />);
-
-  expect(wrapper).toHaveLength(1);
-});
