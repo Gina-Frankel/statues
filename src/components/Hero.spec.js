907b25da5398dbe7141165ccbdc80f3d4a4f65c0
Gina Frankel <g.frankel22@gmail.com>
2020-07-08 16:12:35 +0100

    Format  with ESLint

--- src/components/Hero.spec.js
+++ src/components/Hero.spec.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import {Hero} from './Hero';
+import React from "react";
+import { shallow } from "enzyme";
+import { Hero } from "./Hero";
 
 describe("Hero", () => {
-  it ('displays app name', () => {
-    const wrapper = shallow(<Hero/>);
-    
-    expect(wrapper.text()).toContain("Don't stand for art that doesn't stand for you")
-  
+  it("displays app name", () => {
+    const wrapper = shallow(<Hero />);
+
+    expect(wrapper.text()).toContain(
+      "Don't stand for art that doesn't stand for you"
+    );
   });
-  it ('displays tag line', () => {
-    const wrapper = shallow(<Hero/>);
-    
-    expect(wrapper.text()).toContain("There are dozens of public artworks depicting people involved in slavery")
-  
+  it("displays tag line", () => {
+    const wrapper = shallow(<Hero />);
+
+    expect(wrapper.text()).toContain(
+      "There are dozens of public artworks depicting people involved in slavery"
+    );
   });
 });
