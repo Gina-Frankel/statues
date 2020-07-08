b4b3bc4312a84764ce5641172639ae3085282906
Gina Frankel <g.frankel22@gmail.com>
2020-07-08 19:00:47 +0100

    Add jumbotron styling

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,15 +3,20 @@ import genericStatuePicture from "../statue.png";
 
 export function Hero() {
   return (
-    <div className="jumbotron">
-      <h1 className="display-4">
-        Don't stand for art that doesn't stand for you
-      </h1>
-      <p className="lead">
-        There are dozens of public artworks depicting people involved in slavery
-      </p>
-      <div className="hero-picture">
-        <img src={genericStatuePicture} alt="Generic problematic statue"></img>
+    <div>
+      <img
+        src={genericStatuePicture}
+        className="float-right"
+        alt="Generic problematic statue"
+      ></img>
+      <div className="jumbotron px-5 mt-5 col-9">
+        <h1 className="display-4">
+          Don't stand for art that doesn't stand for you
+        </h1>
+        <p className="lead col-9">
+          There are dozens of public artworks depicting people involved in
+          slavery and other terrible acts.
+        </p>
       </div>
     </div>
   );
