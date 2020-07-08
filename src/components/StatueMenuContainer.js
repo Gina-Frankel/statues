c3666ece8d4df8ef90d1b9286fb1a10c85e629d9
Gina Frankel <g.frankel22@gmail.com>
2020-07-08 11:50:14 +0100

    Change imported component to correct name

--- src/components/StatueMenuContainer.js
+++ src/components/StatueMenuContainer.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import { StatueList } from "./StatueMenu";
+import { StatueMenu } from "./StatueMenu";
 
 export class StatueMenuContainer extends Component {
   render() {
     return (
       <div>
         <h1>Statues</h1>
-        <StatueList />
+        <StatueMenu />
       </div>
     );
   }
