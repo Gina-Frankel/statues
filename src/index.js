e9bba61041849e35a0fe3594ce3757909332c1ae
Gina Frankel <g.frankel22@gmail.com>
2020-07-08 16:11:11 +0100

    Delete  comment + remove extra lines

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import 'bootstrap/dist/css/bootstrap.css';
-import './fonts/edosz.ttf'
-// import * as serviceWorker from './serviceWorker';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "bootstrap/dist/css/bootstrap.css";
+import "./fonts/edosz.ttf";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
