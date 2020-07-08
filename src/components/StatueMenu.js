6b6109a7ee36f7f18c4282c08d57e9f80f614ccd
Gina Frankel <g.frankel22@gmail.com>
2020-07-08 11:49:18 +0100

    Change imported component to correct name

--- src/components/StatueMenu.js
+++ src/components/StatueMenu.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { StatueListItem } from "./StatueListItem";
+import { StatueMenuItem } from "./StatueMenuItem";
 import slaverStatueList from "../statues/slavers.js";
 
 export class StatueMenu extends Component {
@@ -11,7 +11,7 @@ export class StatueMenu extends Component {
   render() {
     const statues = slaverStatueList;
     const items = statues.map((item) => (
-      <StatueListItem key={item.name} value={item} />
+      <StatueMenuItem key={item.name} value={item} />
     ));
 
     return <ul>{items}</ul>;
