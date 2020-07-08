0007d79bf2c4c387f1bbe864720a68d83e1d019b
Gina Frankel <g.frankel22@gmail.com>
2020-07-08 19:01:38 +0100

    Delete HeroPicture component + delete Hero styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,27 @@
-import React from 'react';
-import {NavBar} from './NavBar.js'
-import {Hero} from './Hero.js'
-import {HeroPicture} from './HeroPicture.js'
-import StatueListContainer from './StatueListContainer.js'
-import './App.css';
-import '../index.css';
+import React from "react";
+
+import { Hero, NavBar } from "./index.js";
+
+//import { NavBar } from "./NavBar.js";
+//import { Hero } from "./Hero.js";
+
+// import { HeroPicture } from "./HeroPicture.js";
+import StatueListContainer from "./StatueListContainer.js";
+import "./App.css";
+import "../index.css";
+import "./index.js";
 
 function App() {
   return (
     <div className="app">
       <header className="app-header">
-        <NavBar/>
+        <NavBar />
       </header>
-      <div className="Hero-container">
-        <div className="row justify-content-around">
-          <div className="col-lg-6 text-center">
-          <Hero/>
-          </div>
-          <div className="col-lg-5 text-center">
-          <HeroPicture/>
-          </div>
-        </div>
-      </div>
+
+      <Hero />
+
       <div>
-        <StatueListContainer/>
+        <StatueListContainer />
       </div>
     </div>
   );
