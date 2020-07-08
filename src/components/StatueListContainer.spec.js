02d36d9c2e478cf2231b70eaaba206335a64e817
Gina Frankel <g.frankel22@gmail.com>
2020-07-08 11:38:12 +0100

    Change component  names to StatueMenuContainer

--- src/components/StatueListContainer.spec.js
+++ src/components/StatueListContainer.spec.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import { StatueListContainer } from './StatueListContainer';
+import React from "react";
+import { shallow } from "enzyme";
+import { StatueMenuContainer } from "./StatueMenuContainer";
 
-it('renders without crashing', () => {
-  const wrapper = shallow(< StatueListContainer />);
+it("renders without crashing", () => {
+  const wrapper = shallow(<StatueMenuContainer />);
 
-  expect(wrapper).toHaveLength(1)
-})
+  expect(wrapper).toHaveLength(1);
+});
 
-it("displays the title for the list of statues", ()=> {
-  const wrapper = shallow(< StatueListContainer />);
+it("displays the title for the list of statues", () => {
+  const wrapper = shallow(<StatueMenuContainer />);
 
-  expect(wrapper.text()).toContain("Statues")
-})
+  expect(wrapper.text()).toContain("Statues");
+});
