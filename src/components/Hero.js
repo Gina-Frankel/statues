3bf8cafb8a78d460a5ab6c005c082a4afadc3f6a
Gina Frankel <g.frankel22@gmail.com>
2020-07-08 16:11:56 +0100

    Delete comment + lint with ESLint

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,15 +1,12 @@
-import React from 'react';
-
+import React from "react";
 
 export function Hero() {
   return (
     <div className="Hero">
-    
-    <h1>Don't stand for art that doesn't stand for you</h1>
-    <p>There are dozens of public artworks depicting people involved in slavery</p>
-   
+      <h1>Don't stand for art that doesn't stand for you</h1>
+      <p>
+        There are dozens of public artworks depicting people involved in slavery
+      </p>
     </div>
   );
 }
-
-//export default Hero;
