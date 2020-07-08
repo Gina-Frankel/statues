b8380d59cc41372fe5d2fc4ea9bf2403a04f55ae
Gina Frankel <g.frankel22@gmail.com>
2020-07-08 18:22:34 +0100

    Add image to render

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,18 @@
 import React from "react";
+import genericStatuePicture from "../statue.png";
 
 export function Hero() {
   return (
-    <div className="Hero">
-      <h1>Don't stand for art that doesn't stand for you</h1>
-      <p>
+    <div className="jumbotron">
+      <h1 className="display-4">
+        Don't stand for art that doesn't stand for you
+      </h1>
+      <p className="lead">
         There are dozens of public artworks depicting people involved in slavery
       </p>
+      <div className="hero-picture">
+        <img src={genericStatuePicture} alt="Generic problematic statue"></img>
+      </div>
     </div>
   );
 }
