7b4b9d125b442cdbf927263a6bf5f38d62c0c9e9
Gina Frankel <g.frankel22@gmail.com>
2020-07-09 13:03:53 +0100

    Replace hardcoded text with JS object values

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,6 @@
 import React from "react";
 import genericStatuePicture from "../statue.png";
+import labels from "../labels";
 
 export function Hero() {
   return (
@@ -10,13 +11,8 @@ export function Hero() {
         alt="Generic problematic statue"
       ></img>
       <div className="jumbotron px-5 mt-5 col-9">
-        <h1 className="display-4">
-          Don't stand for art that doesn't stand for you
-        </h1>
-        <p className="lead col-9">
-          There are dozens of public artworks depicting people involved in
-          slavery and other terrible acts.
-        </p>
+        <h1 className="display-4">{labels.heroHeading}</h1>
+        <p className="lead col-9">{labels.heroSubHeading}</p>
       </div>
     </div>
   );
