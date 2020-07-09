4f5675f90e713a851e7d9cdb7c6a74985cb6d639
Gina Frankel <g.frankel22@gmail.com>
2020-07-09 12:22:41 +0100

    Change to header element size

--- src/components/StatueListContainer.js
+++ src/components/StatueListContainer.js
@@ -3,11 +3,10 @@ import StatueList from "./Statuelist";
 import slaverStatueList from "./../statues/slavers.js";
 
 export class StatueListContainer extends Component {
-
   render() {
     return (
       <div>
-        <h1>Statues</h1>
+        <h2>Statues</h2>
         <StatueList statue={slaverStatueList} />
       </div>
     );
