e032054d03b1e867d3bcc77baab0ce75074bf949
Gina Frankel <g.frankel22@gmail.com>
2020-07-14 18:41:54 +0100

    Fix name changes

--- src/components/StatueRoutes.spec.js
+++ src/components/StatueRoutes.spec.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { shallow } from "enzyme";
-import { StatueRoute } from "./index";
+import { StatueRoutes } from "./index";
 
 it("renders without crashing", () => {
-  const wrapper = shallow(<StatueRoute />);
+  const wrapper = shallow(<StatueRoutes />);
 
   expect(wrapper).toHaveLength(1);
 });
