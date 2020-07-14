985507db39b1b7e7e7eb9867cd040b8e3355b003
Gina Frankel <g.frankel22@gmail.com>
2020-07-14 18:40:13 +0100

    Delete  unnecessary index

--- src/components/StatueMenu.js
+++ src/components/StatueMenu.js
@@ -3,11 +3,10 @@ import { StatueMenuItem } from "./StatueMenuItem";
 import slaverStatueList from "../statues/slavers.js";
 
 export class StatueMenu extends Component {
-
   render() {
     const statues = slaverStatueList;
     const items = statues.map((item, index) => (
-      <StatueMenuItem key={index} value={item} index={index} />
+      <StatueMenuItem key={index} value={item} />
     ));
 
     return <ul>{items}</ul>;
