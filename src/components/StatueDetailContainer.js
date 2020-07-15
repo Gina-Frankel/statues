a833b75af1f92c060598d4fef220b46e41f0da1c
Sophia Bell <sophbell87@gmail.com>
2020-07-15 16:49:11 +0100

    default pic

--- src/components/StatueDetailContainer.js
+++ src/components/StatueDetailContainer.js
@@ -9,10 +9,9 @@ export class StatueDetailContainer extends Component {
       <div className="col-lg-7 col-sm-6 mt-4">
         <div className="row mt-2">
           <h2 className="mb-4">{name}</h2>
-          <div
-            className="col-sm-5"
-            style={{ backgroundColor: "grey", height: "400px" }}
-          ></div>
+          <div className="col-sm-5">
+            <img src="../../default.png" />
+          </div>
           <div className="col-sm-7 container">
             <div className="mt-3 px-3">{details}</div>
           </div>
