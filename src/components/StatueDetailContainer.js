bcc4fd8eb37e50b6d698585a10a6d47a8cbb896a
Sophia Bell <sophbell87@gmail.com>
2020-07-16 15:49:36 +0100

    change col med 5 for image

--- src/components/StatueDetailContainer.js
+++ src/components/StatueDetailContainer.js
@@ -9,7 +9,7 @@ export class StatueDetailContainer extends Component {
       <div className="col-lg-9 col-md-12">
         <div className="row mt-2">
           <h2 className="mb-4">{name}</h2>
-          <div className="col-sm-5">
+          <div className="col-md-5">
             <img src="../../default.png" alt="default" />
           </div>
           <div className="col-sm-12 col-md-7 container">
