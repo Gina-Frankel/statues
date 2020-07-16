26ad3334b60c3f90685fa63ef522293026f9d3f2
Sophia Bell <sophbell87@gmail.com>
2020-07-16 15:32:57 +0100

    correcting changes for PR request

--- src/components/StatueMenuContainer.js
+++ src/components/StatueMenuContainer.js
@@ -5,7 +5,7 @@ import style from "./StatueMenuContainer.module.css";
 export class StatueMenuContainer extends Component {
   render() {
     return (
-      <div className="col-lg-3 col-md-6 col-sm-12">
+      <div className="col-lg-3 col-md-12">
         <div className={style.container}>
           <h2 className="pt-3">Statues</h2>
           <StatueMenu />
