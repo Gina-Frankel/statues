b2384cb01c3b760fd7332079af8b8e60f2052e8d
Gina Frankel <g.frankel22@gmail.com>
2020-07-16 17:05:09 +0100

    Export  About component

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { Hero } from "./Hero.js";
 export { NavContainer } from "./NavContainer";
 export { StatueDetailContainer } from "./StatueDetailContainer";
 export { StatueRoutes } from "./StatueRoutes";
+export { About } from "./About";
