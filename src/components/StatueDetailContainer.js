4b09604cc9ed6c5893c539eef5996217157e1576
Sophia Bell <sophbell87@gmail.com>
2020-07-16 15:12:55 +0100

    adding alt tag to image tage

--- src/components/StatueDetailContainer.js
+++ src/components/StatueDetailContainer.js
@@ -10,7 +10,7 @@ export class StatueDetailContainer extends Component {
         <div className="row mt-2">
           <h2 className="mb-4">{name}</h2>
           <div className="col-sm-5">
-            <img src="../../default.png" />
+            <img src="../../default.png" alt="default" />
           </div>
           <div className="col-sm-12 col-md-7 container">
             <div>{details}</div>
