6dcb086fdf8c7def789413c5156a2fec5b3e0df0
Harry Turnbull <hturnbull@live.co.uk>
2020-07-20 15:26:11 +0100

    Use statueId to create unique slugs for routes in StatueRoutes

--- src/components/StatueRoutes.js
+++ src/components/StatueRoutes.js
@@ -6,7 +6,8 @@ import { BrowserRouter as Route, Switch } from "react-router-dom";
 export class StatueRoutes extends Component {
   render() {
     const newRoute = SlaverStatueList.map((statue, index) => {
-      const url = `/StatueDetailContainer/${statue.name.replace(/\s/g, "-")}`;
+      const slug = `${statue.name.replace(/\s/g, "-")}-${statue.statueId}`;
+      const url = `/StatueDetailContainer/${slug}`;
       return (
         <Route key={index} path={url}>
           <StatueDetailContainer statue={statue} />
