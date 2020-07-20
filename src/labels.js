bc64c7a86546aebd66d1341e7ff78bcccdb4bda2
Gina Frankel <g.frankel22@gmail.com>
2020-07-20 14:56:07 +0100

    Add labels for About page

--- src/labels.js
+++ src/labels.js
@@ -2,6 +2,16 @@ const labels = {
   heroHeading: "Don't stand for art that doesn't stand for you",
   heroSubHeading:
     "There are dozens of public artworks depicting people involved in slavery and other terrible acts.",
+  AboutHeading: "About the project",
+  AboutMissionTitle: "Why",
+  AboutMissionTitleText:
+    "Recently a statue of Edward Colston was removed by protestors and sent into the sea.  We had never known there was statue in Bristol of a slave trader. This made us  realise that we walk past all this public art and have no idea about the people they represent and if these celebrated people represent values that we share. We want people to have a deeper understanding of the art that surrounds them,  and have an opportunity to reflect on the values represented by the statue , the artist and if these values have a place in our cultural identity.",
+  AboutDataTitle: "Data Collection",
+  AboutData: "Pyhton blah blah",
+  AboutFrontEndTitle: "Front End",
+  AboutFrontEnd:
+    "We choose to use React partly because of the learning experience, and because as we were not sure of how large the project would be we needed something scalable.",
+  AboutThanks:
+    "Thanks to Ben Furber  for his continious support and thorough peer reviews.",
 };
-
 export default labels;
