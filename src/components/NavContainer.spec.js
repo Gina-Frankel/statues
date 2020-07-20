5855f78137c731af796b24416ea22c0bc583b6e7
Harry Turnbull <hturnbull@live.co.uk>
2020-07-20 16:00:35 +0100

    R: NavContainer renders a Link to the About component/page

--- src/components/NavContainer.spec.js
+++ src/components/NavContainer.spec.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { shallow } from "enzyme";
 import { NavContainer } from "./NavContainer";
+import { Link } from "react-router-dom";
 
-it("renders correctly", () => {
-  const wrapper = shallow(<NavContainer />);
+describe("NavContainer", () => {
+  it("renders correctly", () => {
+    const wrapper = shallow(<NavContainer />);
+    expect(wrapper).toHaveLength(1);
+  });
 
-  expect(wrapper).toHaveLength(1);
+  it("renders a Link to the About component/page", () => {
+    const wrapper = shallow(<NavContainer />);
+    const link = <Link to="/about">About</Link>;
+    expect(wrapper.containsMatchingElement(link)).toEqual(true);
+  });
 });
