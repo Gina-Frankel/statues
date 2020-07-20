0372ec19d382138e976ae8523cd2685877fb84d4
Harry Turnbull <hturnbull@live.co.uk>
2020-07-20 15:28:28 +0100

    RM extra div

--- src/components/App.js
+++ src/components/App.js
@@ -13,18 +13,16 @@ function App() {
       <header className="App-header">
         <NavContainer />
       </header>
-      <div>
-        <div className="container">
-          <div className="row justify-content-end">
-            <Hero />
-          </div>
-          <Router>
-            <div className="row justify-content-between mt-3">
-              <StatueMenuContainer />
-              <StatueRoutes />
-            </div>
-          </Router>
+      <div className="container">
+        <div className="row justify-content-end">
+          <Hero />
         </div>
+        <Router>
+          <div className="row justify-content-between mt-3">
+            <StatueMenuContainer />
+            <StatueRoutes />
+          </div>
+        </Router>
       </div>
     </div>
   );
