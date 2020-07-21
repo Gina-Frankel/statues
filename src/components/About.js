80c607d13d84a1ba0818c3b776c172380e1b3699
Gina Frankel <g.frankel22@gmail.com>
2020-07-21 15:26:50 +0100

    Add rendering of githubLinks

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,22 @@
 import React from "react";
 import labels from "../labels";
+import developers from "../developers";
 
 export function About() {
+  const githubLinks = developers.map((developer) => {
+    const githubUrl = "https://github.com/" + developer.githubUsername;
+    return (
+      <li key={developer.githubUsername}>
+        <a href={githubUrl}> {developer.name}</a>
+      </li>
+    );
+  });
   return (
     <>
       <div>{labels.aboutHeading}</div>
       <div>{labels.aboutMissionTitle}</div>
       <div>{labels.aboutMissionTitleText}</div>
+      <ul>{githubLinks}</ul>
     </>
   );
 }
