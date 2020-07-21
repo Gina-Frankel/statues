541c59f600828559188e55576b9a1a9707745fdb
Gina Frankel <g.frankel22@gmail.com>
2020-07-21 16:48:03 +0100

    Destructure props.value

--- src/components/StatueMenuItem.js
+++ src/components/StatueMenuItem.js
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom";
 import style from "./StatueMenuItem.module.css";
 
 export function StatueMenuItem(props) {
-  const name = props.value.name;
-  const statueId = props.value.statueId;
+  const { name, statueId } = props.value;
+
   const nameNoSpaces = `${name.replace(/\s/g, "-")}-${statueId}`;
   const statueUrl = "/StatueDetailContainer/" + nameNoSpaces;
 
   return (
     <li className={style.li}>
       <Link to={statueUrl} className={style.a}>
-        {props.value.name}
+        {name}
       </Link>
     </li>
   );
