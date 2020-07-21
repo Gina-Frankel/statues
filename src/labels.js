70a36e52396a73cabefa3fbf1968cbc60a03a1e9
Gina Frankel <g.frankel22@gmail.com>
2020-07-21 16:39:42 +0100

    Change to two objects about + hero

--- src/labels.js
+++ src/labels.js
@@ -1,25 +1,22 @@
-const labels = {
-  hero: {
-    heading: "Don't stand for art that doesn't stand for you",
-    subHeading:
-      "There are dozens of public artworks depicting people involved in slavery and other terrible acts.",
-  },
-
-  about: {
-    heading: "About the project",
-    mission: {
-      title: "Why",
-      text:
-        "We created this project to enable people to learn about slavery through examining public art in England. The project seeks to provide information about statues that represent people involved in slavery. Through looking at these people in more detail an understanding about the time-scale of slavery, and the context and values which enabled slavery can be built. Although slavery is abolished pieces of the past still exist today.",
-    },
-    data: { title: "Data Collection", text: "Pyhton blah blah" },
-    frontEnd: {
-      title: "Front End",
-      text:
-        "We choose to use React partly because of the learning experience, and because as we were not sure of how large the project would be we needed something scalable.",
-    },
-    thanks:
-      "Thanks to Ben Furber for his continuous support and thorough peer reviews.",
-  },
+export const hero = {
+  heading: "Don't stand for art that doesn't stand for you",
+  subHeading:
+    "There are dozens of public artworks depicting people involved in slavery and other terrible acts.",
 };
-export default labels;
+
+export const about = {
+  heading: "About the project",
+  mission: {
+    title: "Why",
+    text:
+      "We created this project to enable people to learn about slavery through examining public art in England. The project seeks to provide information about statues that represent people involved in slavery. Through looking at these people in more detail an understanding about the time-scale of slavery, and the context and values which enabled slavery can be built. Although slavery is abolished pieces of the past still exist today.",
+  },
+  data: { title: "Data Collection", text: "Pyhton blah blah" },
+  frontEnd: {
+    title: "Front End",
+    text:
+      "We choose to use React partly because of the learning experience, and because as we were not sure of how large the project would be we needed something scalable.",
+  },
+  thanks:
+    "Thanks to Ben Furber for his continuous support and thorough peer reviews.",
+};
