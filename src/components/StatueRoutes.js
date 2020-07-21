e63996f9bca10d5b76e11a3c3f93ae2994f3cae0
Gina Frankel <g.frankel22@gmail.com>
2020-07-21 16:54:24 +0100

    Destructure statue

--- src/components/StatueRoutes.js
+++ src/components/StatueRoutes.js
@@ -6,7 +6,8 @@ import { BrowserRouter as Route, Switch } from "react-router-dom";
 export class StatueRoutes extends Component {
   render() {
     const newRoute = SlaverStatueList.map((statue, index) => {
-      const slug = `${statue.name.replace(/\s/g, "-")}-${statue.statueId}`;
+      const { name, statueId } = statue;
+      const slug = `${name.replace(/\s/g, "-")}-${statueId}`;
       const url = `/StatueDetailContainer/${slug}`;
       return (
         <Route key={index} path={url}>
