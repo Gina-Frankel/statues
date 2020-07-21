4a029d314596c1cd6e0b78deeabef7e679ab4af6
Gina Frankel <g.frankel22@gmail.com>
2020-07-21 16:03:43 +0100

    Add nesting of mission, data + frontEnd

--- src/labels.js
+++ src/labels.js
@@ -7,14 +7,17 @@ const labels = {
 
   about: {
     heading: "About the project",
-    missionTitle: "Why",
-    missionText:
-      "We created this project to enable people to learn about slavery through examining public art in England. The project seeks to provide information about statues that represent people involved in slavery. Through looking at these people in more detail an understanding about the time-scale of slavery, and the context and values which enabled slavery can be built. Although slavery is abolished pieces of the past still exist today.",
-    dataTitle: "Data Collection",
-    dataText: "Pyhton blah blah",
-    frontEndTitle: "Front End",
-    frontEndText:
-      "We choose to use React partly because of the learning experience, and because as we were not sure of how large the project would be we needed something scalable.",
+    mission: {
+      title: "Why",
+      text:
+        "We created this project to enable people to learn about slavery through examining public art in England. The project seeks to provide information about statues that represent people involved in slavery. Through looking at these people in more detail an understanding about the time-scale of slavery, and the context and values which enabled slavery can be built. Although slavery is abolished pieces of the past still exist today.",
+    },
+    data: { title: "Data Collection", text: "Pyhton blah blah" },
+    frontEnd: {
+      title: "Front End",
+      text:
+        "We choose to use React partly because of the learning experience, and because as we were not sure of how large the project would be we needed something scalable.",
+    },
     thanks:
       "Thanks to Ben Furber for his continuous support and thorough peer reviews.",
   },
