cfb59eff7d6f0ea80839b879729eb7c7cab520cd
Gina Frankel <g.frankel22@gmail.com>
2020-07-21 15:59:49 +0100

    Add nesting of about and hero

--- src/labels.js
+++ src/labels.js
@@ -1,17 +1,22 @@
 const labels = {
-  heroHeading: "Don't stand for art that doesn't stand for you",
-  heroSubHeading:
-    "There are dozens of public artworks depicting people involved in slavery and other terrible acts.",
-  aboutHeading: "About the project",
-  aboutMissionTitle: "Why",
-  aboutMissionTitleText:
-    "We created this project to enable people to learn about slavery through examining public art in England. The project seeks to provide information about statues that represent people involved in slavery. Through looking at these people in more detail an understanding about the time-scale of slavery, and the context and values which enabled slavery can be built. Although slavery is abolished pieces of the past still exist today.",
-  aboutDataTitle: "Data Collection",
-  aboutData: "Pyhton blah blah",
-  aboutFrontEndTitle: "Front End",
-  aboutFrontEnd:
-    "We choose to use React partly because of the learning experience, and because as we were not sure of how large the project would be we needed something scalable.",
-  aboutThanks:
-    "Thanks to Ben Furber for his continuous support and thorough peer reviews.",
+  hero: {
+    heading: "Don't stand for art that doesn't stand for you",
+    subHeading:
+      "There are dozens of public artworks depicting people involved in slavery and other terrible acts.",
+  },
+
+  about: {
+    heading: "About the project",
+    missionTitle: "Why",
+    missionText:
+      "We created this project to enable people to learn about slavery through examining public art in England. The project seeks to provide information about statues that represent people involved in slavery. Through looking at these people in more detail an understanding about the time-scale of slavery, and the context and values which enabled slavery can be built. Although slavery is abolished pieces of the past still exist today.",
+    dataTitle: "Data Collection",
+    dataText: "Pyhton blah blah",
+    frontEndTitle: "Front End",
+    frontEndText:
+      "We choose to use React partly because of the learning experience, and because as we were not sure of how large the project would be we needed something scalable.",
+    thanks:
+      "Thanks to Ben Furber for his continuous support and thorough peer reviews.",
+  },
 };
 export default labels;
