dd39d3d440777ed4e71f78a91089787487c0b72c
Gina Frankel <g.frankel22@gmail.com>
2020-07-21 15:09:11 +0100

    Add developers object with developer details

---
+++ src/developers.js
@@ -0,0 +1,5 @@
+const developers = [
+  { name: "Harry Turnbull", githubName: "hturnbull93" },
+  { name: "Gina Frankel", githubName: "gina-frankel" },
+  { name: "Sophia Bell", githubName: "kittaru87" },
+];
