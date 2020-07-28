b06b30e2e16b276ddffe1208ea35ade361fd82e3
Gina Frankel <g.frankel22@gmail.com>
2020-07-28 11:55:44 +0100

    Render summary + actual image + Style padding

--- src/components/StatueDetailContainer.js
+++ src/components/StatueDetailContainer.js
@@ -3,25 +3,20 @@ import "./StatueDetailContainer.module.css";
 
 export class StatueDetailContainer extends Component {
   render() {
-    const { name, wikiArticleURI, artworkURI } = this.props.statue;
+    const {
+      name,
+      wikiArticleURI,
+      artworkURI,
+      slaverSummary,
+      image,
+    } = this.props.statue;
 
     return (
       <div className="col-lg-9 col-md-12">
         <div className="row mt-2">
           <h2 className="mb-4">{name}</h2>
-          <div className="col-md-5">
-            <img src="../../default.png" alt="default" />
-          </div>
-          <div className="col-sm-12 col-md-7 container">
-            Information about {name} can be found at:{" "}
-            <a
-              href={wikiArticleURI}
-              target="_blank"
-              rel="noopener noreferrer"
-              alt={name}
-            >
-              Wikipedia article
-            </a>
+          <div className="col-md-5 pb-4">
+            <img src={image} alt="default" />
             Information about the artwork can be found at:{" "}
             <a
               href={artworkURI}
@@ -30,6 +25,18 @@ export class StatueDetailContainer extends Component {
               alt={name}
             >
               ArtUK page
+            </a>
+          </div>
+          <div className="col-sm-12 col-md-7 container">
+            <p>{slaverSummary}</p>
+            More Information about {name} can be found at:{" "}
+            <a
+              href={wikiArticleURI}
+              target="_blank"
+              rel="noopener noreferrer"
+              alt={name}
+            >
+              Wikipedia article
             </a>
           </div>
         </div>
