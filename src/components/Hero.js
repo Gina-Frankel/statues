e1d6439be0a1ba553edb79ba05f472d81ebc8dd7
Gina Frankel <60652902+Gina-Frankel@users.noreply.github.com>
2020-07-30 08:11:33 +0100

    Revert "Delete text"

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,6 +9,7 @@ export function Hero() {
       <div className="jumbotron mt-5 col-lg-5 col-sm-12 ">
         <h1 className="display-4  ">{heading}</h1>
         <p className="lead col-9">{subHeading}</p>
+        <p> find out more</p>
       </div>
       <div className="col-lg-7 col-sm-12 align-self-end text-right">
         <img src={genericStatuePicture} alt="Generic problematic statue"></img>
